43eac20e6b4f43ff5092f4d75a841fe31db1cedb
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-04 16:15:00 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace C_Todo
+{
+    class Program
+    {
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string projectDir = Directory.GetCurrentDirectory();
+            string storage = projectDir + "/SavedTodos.txt";
+
+            string newTodo = string.Join(" ", args);
+
+            
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@storage, true))
+            {
+                file.WriteLine($"{newTodo}");
+            }
+        }
+    }
+}
